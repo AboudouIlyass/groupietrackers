e74eab631f7bcad63126b89ed2915de9b38ef3cd
AboudouIlyass <ilyassaboudou@gmail.com>
2025-07-28 16:34:08 +0100

    style

--- gt/handlers/artistsinfo.go
+++ gt/handlers/artistsinfo.go
@@ -3,4 +3,9 @@ package handlers
 import "net/http"
 
 func ArtistsInfo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		utils.ErrorPage(w, http.StatusMethodNotAllowed)
+		return
+	}
+	utils.ParseAndExecute(w, "templates/html/artistsinfo.html", config.Fulldata)
 }
